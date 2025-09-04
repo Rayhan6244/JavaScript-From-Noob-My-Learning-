d5b0095ee216fc42ae2b80eea5b08a71cabb13b1
Rayhan Alam Towhid <rayhanalamtowhid@gmail.com>
2025-09-05 02:11:25 +0600

    Create arrow_with_const.js

---
+++ arrow_with_const.js
@@ -0,0 +1,13 @@
+const name = 'Rayhan';
+const age = 23;
+const hobby = true;
+
+const rahu = (username, userage, userhobby) => {
+    return('Name is: ' + username + ', User Age is: ' + userage + ', His hobby is: ' + userhobby);
+}
+
+console.log(rahu(name, age, hobby));
+
+const add = (r, i) => ('The sum is: ' + (r + i));
+
+console.log(add(1, 2));
