b54fa8ba7447ed0254d2197fee63c1eba91de80f
Rayhan Alam Towhid <rayhanalamtowhid@gmail.com>
2025-09-05 02:12:01 +0600

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,8 @@
+//Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+let country = "Bangladesh";
+let continent = "ASIA";
+let population = 20000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
