302c524263f88f8254e1b5bbd84c6bd3df7b766a
Rayhan Alam Towhid <rayhanalamtowhid@gmail.com>
2025-09-05 02:09:22 +0600

    Create var.js

---
+++ var.js
@@ -0,0 +1,10 @@
+var name = 'Rayhan';
+var age = 23;
+var hobbies = true;
+
+function rahu(username, userage, userhobby){
+    return(
+        'Name is ' + username + ', Age is ' + userage + ', His Hobby is ' + userhobby
+    );
+}
+console.log(rahu(name, age, hobbies));
