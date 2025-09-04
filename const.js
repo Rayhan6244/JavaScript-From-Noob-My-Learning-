cd522f87ec4608f70cfae48390f8a8e35c7972f4
Rayhan Alam Towhid <rayhanalamtowhid@gmail.com>
2025-09-05 02:10:08 +0600

    Create const.js

---
+++ const.js
@@ -0,0 +1,10 @@
+const name = 'Rayhan';
+const age = 23;
+const hobby = true;
+
+function rahu(username, userage, userhobby){
+    return('Name is: ' + username + ', User Age is: ' + userage + ', His hobby is: ' + userhobby);
+}
+
+console.log(rahu(name, age, hobby));
+
