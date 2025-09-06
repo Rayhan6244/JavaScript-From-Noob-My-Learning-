03ba3adb54600ef672a9a5a233d1a8d9994d2c35
Rayhan Alam Towhid <rayhanalamtowhid@gmail.com>
2025-09-07 04:41:09 +0600

    Create hudai.js

---
+++ hudai.js
@@ -0,0 +1,9 @@
+let x,y,z;
+x = `Kire Towhid. Ki obostha?`
+y = 23;
+z = true;
+document.getElementById('p1').textContent = x;
+document.getElementById('p2').textContent = `Tor boyosh ${y} bochor`;
+document.getElementById('p3').textContent = `Tui ki student?: ${z}`;
+
+//console.log(x, `Tor boyosh ${y} bochor`);
